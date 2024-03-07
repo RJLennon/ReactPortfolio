dd7c5f972cd88d6285db1b1995e54e6d940b7790
RJLennon <robjohnlennon@gmail.com>
2024-03-06 23:33:20 -0500

    added starter code for react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import 'materialize-css/dist/css/materialize.min.css'
 
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        This is the home page. You can use the navigation bar to visit the other
+        pages.
+      </p>
+    </div>
+  );
+}
