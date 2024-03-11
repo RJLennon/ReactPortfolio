9898ee1045df28c2fdd6e1c6efb644c0d7e81740
RJLennon <robjohnlennon@gmail.com>
2024-03-10 23:12:09 -0400

    updated contact page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
-import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 import ErrorPage from './pages/ErrorPage';
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+import M from 'materialize-css'; // Import Materialize CSS library
+
+
 export default function ContactPage() {
+  useEffect(() => {
+    // Initialize Materialize components
+    M.AutoInit();
+    // Or initialize specific components like text inputs
+    // M.FormSelect.init(document.querySelectorAll('select'));
+  }, []); // Empty dependency array ensures that this effect runs only once
+
     return (
       <div>
         <h1>Welcome to the Contact Page</h1>
@@ -6,6 +17,35 @@ export default function ContactPage() {
           This is the contact page. You can use the navigation bar to visit the other
           pages.
         </p>
+        <div className="row">
+  <form className="col s12">
+    <div className="row">
+      <div className="input-field col s6">
+        <input id="first_name" type="text" className="validate" />
+        <label htmlFor="first_name">First Name</label>
+      </div>
+      <div className="input-field col s6">
+        <input id="last_name" type="text" className="validate" />
+        <label htmlFor="last_name">Last Name</label>
+      </div>
+    </div>
+    <div className="row">
+      <div className="input-field col s12">
+        <input id="email" type="email" className="validate" />
+        <label htmlFor="email">Email</label>
+      </div>
+    </div>
+    <div className="row">
+      <div className="input-field col s12">
+        <textarea id="message" className="materialize-textarea"></textarea>
+        <label htmlFor="message">Message</label>
+      </div>
+    </div>
+    <button className="btn waves-effect waves-light" type="submit" name="action">
+      <i className="material-icons right">send</i>
+    </button>
+  </form>
+</div>
       </div>
     );
   }
