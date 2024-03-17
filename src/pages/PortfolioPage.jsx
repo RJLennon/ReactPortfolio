38d7655e080f5775c7069d6372f3929335e80300
RJLennon <robjohnlennon@gmail.com>
2024-03-17 11:19:22 -0400

    updated image urls

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -8,8 +8,8 @@ export default function PortfolioPage() {
         View applications that I have developed.
       </p>
       <div class="row">
-        <Project imageURL="/src/assets/Brewery_Finder.png" projectName="Brewery Finder" projectURL="https://bsaglimb.github.io/BreweryFinder/" projectDescription="Find local breweries"/>
-        <Project imageURL="/src/assets/Employee_Tracker.png" projectName="Employee Tracker" projectURL="https://github.com/RJLennon/Challenge12" projectDescription="Employee database management system"/>
+        <Project imageURL="../assets/Brewery_Finder.png" projectName="Brewery Finder" projectURL="https://bsaglimb.github.io/BreweryFinder/" projectDescription="Find local breweries"/>
+        <Project imageURL="../assets/Employee_Tracker.png" projectName="Employee Tracker" projectURL="https://github.com/RJLennon/Challenge12" projectDescription="Employee database management system"/>
         <Project imageURL="/src/assets/Note_Taker.png" projectName="Note Taker" projectURL="https://rjl-note-taker-5aad7aec2cdc.herokuapp.com/" projectDescription="Note taking application"/>
         <Project imageURL="/src/assets/PersonaPal.png" projectName="PersonaPal" projectURL="https://github.com/RJLennon/PersonaPals" projectDescription="AI powered penpal"/>
         <Project imageURL="/src/assets/Password_Generator.png" projectName="Password Generator" projectURL="https://rjlennon.github.io/Challenge3/" projectDescription="Generate a random password"/>
